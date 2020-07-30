59df384980a98948e6bb8c35701fdc339e41b7cb
Bradly Horn <wolfnchaos@gmail.com>
2020-07-30 04:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
+  Object.assign({}, object, { [key]: value });
   delete object[key];
   return object;
 }
